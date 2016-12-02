791fe99176e9e54e551bc2880d6f60d27fda5cf9
Vladislav <yundin@styleru.org>
2016-12-02 18:11:17 +0300

    GridView inside Fragment

--- app/src/main/java/com/yundin/testfragments1/activity/Fragments.java
+++ app/src/main/java/com/yundin/testfragments1/activity/Fragments.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import com.yundin.testfragments1.R;
+import com.yundin.testfragments1.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +25,32 @@ public class Fragments extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragments);
+
+
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Рыжик", "22", R.drawable.card1));
+        products.add(new Product("Барсик", "66", R.drawable.card2));
+        products.add(new Product("Мурзик", "89", R.drawable.card3));
+        products.add(new Product("Мурка", "77", R.drawable.card4));
+        products.add(new Product("Васька", "78", R.drawable.card5));
+        products.add(new Product("Полосатик", "73", R.drawable.card6));
+        products.add(new Product("Матроскин", "75", R.drawable.card7));
+        products.add(new Product("Лизка", "7754",R.drawable.card8));
+        products.add(new Product("Томосина", "73",R.drawable.card1));
+        products.add(new Product("Бегемот", "747",R.drawable.card1));
+        products.add(new Product("Чеширский", "723",R.drawable.card1));
+        products.add(new Product("Дивуар", "74",R.drawable.card1));
+        products.add(new Product("Тигра", "3457",R.drawable.card1));
+        products.add(new Product("Лаура", "7sdf7",R.drawable.card1));
+        products.add(new Product("Антон", "7sd7",R.drawable.card1));
+        products.add(new Product("Котя", "7df7",R.drawable.card1));
+        products.add(new Product("Стар", "77ZXVF",R.drawable.card1));
+        products.add(new Product("Зимбабве", "7СМЯ7",R.drawable.card1));
+        products.add(new Product("Игорь", "7457",R.drawable.card1));
+        products.add(new Product("Пантера", "7sdf7",R.drawable.card1));
+        products.add(new Product("Тульский", "727",R.drawable.card1));
+        products.add(new Product("Капуста", "727",R.drawable.card1));
 
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
--- app/src/main/java/com/yundin/testfragments1/activity/OneFragment.java
+++ app/src/main/java/com/yundin/testfragments1/activity/OneFragment.java
@@ -5,7 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import com.yundin.testfragments1.R;
+import com.yundin.testfragments1.adapter.ProductAdapter;
+import com.yundin.testfragments1.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vladislav on 02.12.16.
@@ -25,6 +31,37 @@ public class OneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_one, container, false);
+
+
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("Рыжик", "22", R.drawable.card1));
+        products.add(new Product("Барсик", "66", R.drawable.card2));
+        products.add(new Product("Мурзик", "89", R.drawable.card3));
+        products.add(new Product("Мурка", "77", R.drawable.card4));
+        products.add(new Product("Васька", "78", R.drawable.card5));
+        products.add(new Product("Полосатик", "73", R.drawable.card6));
+        products.add(new Product("Матроскин", "75", R.drawable.card7));
+        products.add(new Product("Лизка", "7754",R.drawable.card8));
+        products.add(new Product("Томосина", "73",R.drawable.card1));
+        products.add(new Product("Бегемот", "747",R.drawable.card1));
+        products.add(new Product("Чеширский", "723",R.drawable.card1));
+        products.add(new Product("Дивуар", "74",R.drawable.card1));
+        products.add(new Product("Тигра", "3457",R.drawable.card1));
+        products.add(new Product("Лаура", "7sdf7",R.drawable.card1));
+        products.add(new Product("Антон", "7sd7",R.drawable.card1));
+        products.add(new Product("Котя", "7df7",R.drawable.card1));
+        products.add(new Product("Стар", "77ZXVF",R.drawable.card1));
+        products.add(new Product("Зимбабве", "7СМЯ7",R.drawable.card1));
+        products.add(new Product("Игорь", "7457",R.drawable.card1));
+        products.add(new Product("Пантера", "7sdf7",R.drawable.card1));
+        products.add(new Product("Тульский", "727",R.drawable.card1));
+        products.add(new Product("Капуста", "727",R.drawable.card1));
+
+        GridView gridView = (GridView) v.findViewById(R.id.gridView);
+        gridView.setAdapter(new ProductAdapter(getContext(),products));
+
+        return v;
     }
 }
